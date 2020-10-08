076b8ca901c87d21b157d8629bc10f988e03af98
fenix-react <mhmd.hsny.1992@gmail.com>
2020-10-08 11:06:23 +0330

    new Changes

--- src/UI/Home/HomePage.js
+++ src/UI/Home/HomePage.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router'
-
-
+import React, {Component} from 'react'
+import {Redirect} from 'react-router'
+import './HomePage.scss'
 
 class HomePage extends Component {
 
-    state={
+    state = {
         redirect: false
     }
 
-    render(){
+    render() {
 
-        return(
+        return (
             <React.Fragment>
-            <button onClick={()=>{localStorage.removeItem('token');
-                                 this.setState({redirect: true})}}> Log The Fuck Out</button>
-            {this.state.redirect ? <Redirect to='/'>  </Redirect> : null}
+                <button
+                    className='btn'
+                    onClick={() => {
+                    localStorage.removeItem('token');
+                    this.setState({redirect: true})
+                }}>
+                    Log The Fuck Out</button>
+                {this.state.redirect
+                    ? <Redirect to='/'></Redirect>
+                    : null}
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">HTML</a>
+                        </li>
+                        <li>
+                            <a href="#">CSS</a>
+                        </li>
+                        <li>
+                            <a href="#">SASS</a>
+                        </li>
+                    </ul>
+                </nav>
             </React.Fragment>
         )
     }
